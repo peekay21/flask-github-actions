dff747c09d080215f9033d874a05bc4bc85124f7
PRADIP KUMAR MURMU <46897789+peekay21@users.noreply.github.com>
2024-02-04 05:27:45 +0530

    Created  test_hello file

---
+++ test_hello.py
@@ -0,0 +1,11 @@
+import unittest
+from app import app
+
+class TestHelloWorld(unittest.TestCase):
+  def setUp(self):
+    self.client = app.test_client()
+
+  def test_hello_world(self):
+    response = self.client.get('/')
+    self.assertEqual(response.status_code, 200)
+    self.assertIn(b'Hello, World!', response.data)
